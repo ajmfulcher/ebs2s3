02f46e52959fa21e4fd0b2a59d495af1df5fe1cb
ajmfulcher <andrew.fulcher@gmail.com>
2011-04-10 19:46:41 +0100

    Schedule all defined jobs on startup

--- config/initializers/task_scheduler.rb
+++ config/initializers/task_scheduler.rb
@@ -2,3 +2,11 @@ require 'rubygems'
 require 'rufus/scheduler'
 
 $scheduler = Rufus::Scheduler.start_new
+
+# Schedule all defined jobs on startup
+jobs = Job.all
+jobctl = JobsController.new
+
+jobs.each do |job|
+  jobctl.schedule(job)
+end
